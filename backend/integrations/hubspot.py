5c8f9f6635fd5f39ff886fbd5930ee1e6d03f5d3
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2025-01-20 13:32:40 +0530

    Updated auth url scopes AND added support for load data button

--- backend/integrations/hubspot.py
+++ backend/integrations/hubspot.py
@@ -22,7 +22,7 @@ CLIENT_SECRET = os.getenv('HUBSPOT_CLIENT_SECRET')
 
 
 REDIRECT_URI = 'http://localhost:8000/integrations/hubspot/oauth2callback'
-authorization_url = f'https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri=http://localhost:8000/integrations/hubspot/oauth2callback&scope=oauth'
+authorization_url = f'https://app.hubspot.com/oauth/authorize?client_id={CLIENT_ID}&redirect_uri={REDIRECT_URI}&scope=oauth%20crm.objects.companies.read%20crm.schemas.contacts.read%20crm.objects.contacts.read'
 
 async def authorize_hubspot(user_id, org_id):
     state_data = {
@@ -32,7 +32,6 @@ async def authorize_hubspot(user_id, org_id):
     }
     encoded_state = json.dumps(state_data)
     await add_key_value_redis(f'hubspot_state:{org_id}:{user_id}', encoded_state, expire=600)
-    # print(f'Authorization URL ...............{authorization_url}&state={encoded_state}')
     return f'{authorization_url}&state={encoded_state}'
 
 async def oauth2callback_hubspot(request: Request):
@@ -87,10 +86,34 @@ async def get_hubspot_credentials(user_id, org_id):
 
     return credentials
 
-async def create_integration_item_metadata_object(response_json):
-    # TODO
-    pass
+def create_integration_item_metadata_object(response_json):
+    # print(response_json['id'])
+    properties = response_json['properties']
+    itegration_item_metadata = IntegrationItem(
+        id=response_json['id'],
+        name=" ".join([properties['firstname'], properties['lastname']]),
+        creation_time=properties['createdate'],
+        last_modified_time=properties['lastmodifieddate']
+    )
+    return itegration_item_metadata
 
 async def get_items_hubspot(credentials):
-    # TODO
-    pass
+    """Aggregates all metadata relevant for a HubSpot integration"""
+    credentials = json.loads(credentials)
+    response = requests.get(
+        'https://api.hubapi.com/crm/v3/objects/contacts',
+        headers={
+            'Authorization': f'Bearer {credentials.get("access_token")}',
+            'Content-Type': 'application/json'
+        },
+    )
+
+    list_of_integration_item_metadata = []
+    if response.status_code == 200:
+        results = response.json().get('results', [])
+        for result in results:
+            list_of_integration_item_metadata.append(
+                create_integration_item_metadata_object(result)
+            )
+
+    return list_of_integration_item_metadata
